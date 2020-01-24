8240f2ee24070a6218357c4ae8f0262d4a6fff04
Sam Wen <samw@ritani.com>
2020-01-24 17:59:38 -0500

    set default min and max pool sizes, upgrade drive

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@
 const MongoClient = require('mongodb').MongoClient;
 
 class Mongodb {
-    constructor(url) {
+    constructor(url, minSize = 4, poolSize = 16) {
         this.url = url;
+        this.minSize = minSize;
+        this.poolSize =  poolSize;
         this.client = null;
         this.db = null;
     }
@@ -14,8 +16,9 @@ class Mongodb {
             return this.db;
         }
         this.client = await MongoClient.connect(this.url, { 
-            useNewUrlParser: true, 
-            useUnifiedTopology: true, 
+            useUnifiedTopology: true,
+            minSize: this.minSize,
+            poolSize: this.poolSize,
         });
         this.db = this.client.db();
         return this.db;
